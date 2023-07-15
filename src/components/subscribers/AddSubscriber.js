1fdca079db6a4a6b88defdccad92fe53f1af9026
Sahil <ss6611786@gmail.com>
2023-07-15 13:27:45 +0530

    I updated the useRef() instead of useState()

--- src/components/subscribers/AddSubscriber.js
+++ src/components/subscribers/AddSubscriber.js
@@ -1,16 +1,18 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import './AddSubscriber.css';
 import Container from '../templates/Container';
 import Button from '../templates/Button';
 import ErrorModals from '../templates/ErrorModals';
 
 function AddSubscriber(props) {
-    const [nam, setNam] = useState('');
+    // I have implementing useRef() instead of useState() that why i comment useState() element and update function:-->
+    const nameInputRef = useRef('');
+    // const [nam, setNam] = useState('');
     const [pin, setPin] = useState('');
     const [error, setError] = useState(null);
-    const nameHandler = (e) => {
-        setNam(e.target.value);
-    }
+    // const nameHandler = (e) => {
+    //     setNam(e.target.value);
+    // }
     const pincodeHandler = (e) => {
         setPin(e.target.value);
     }
@@ -21,8 +23,9 @@ function AddSubscriber(props) {
             setError({ title: 'Invalid pin', content: 'You must have enter pin whose length is not greater than 6 and this value must be positive' });
             return
         }
-        props.addSubscriptionHandler(nam, pin);
-        setNam('');
+        props.addSubscriptionHandler(nameInputRef.current.value, pin);
+        // setNam('');
+        nameInputRef.current.value = '';
         setPin('');
     }
     const errorHandler = () => {
@@ -38,7 +41,7 @@ function AddSubscriber(props) {
             <form onSubmit={handleSubmit}>
                 <Container className='input'>
                     <label htmlFor='name'>Name</label>
-                    <input type='text' id='name' placeholder='name' onChange={nameHandler} value={nam} required />
+                    <input ref={nameInputRef} type='text' id='name' placeholder='name' required />
                     <label htmlFor='pincode'>Pincode</label>
                     <input type='number' id='pincode' placeholder='pincode' onChange={pincodeHandler} value={pin} required />
                     <Button type="submit" >Save</Button>
--- src/components/templates/ErrorModals.js
+++ src/components/templates/ErrorModals.js
@@ -1,25 +1,53 @@
 import React from 'react';
 import './ErrorModals.css';
-import Container from './Container'
+import Container from './Container';
+import ReactDOM from 'react-dom';
 import Button from './Button'
-
+const Backdrop = () => {
+    return <div className='backdrop'></div>
+}
+const Overlay = (props) => {
+    return (
+        <Container className='modal'>
+            <header className='header'>
+                <h2>{props.title}</h2>
+            </header>
+            <div>
+                <p className='content'>{props.content}</p>
+            </div>
+            <footer className="actions">
+                <Button onClick={props.onClick}>okay</Button>
+            </footer>
+        </Container>
+    )
+}
 function ErrorModals(props) {
     return (
-        <div className='backdrop'>
+        // <div className='backdrop'>
 
 
-            <Container className='modal'>
-                <header className='header'>
-                    <h2>{props.title}</h2>
-                </header>
-                <div>
-                    <p className='content'>{props.content}</p>
-                </div>
-                <footer className="actions">
-                    <Button onClick={props.onClick}>okay</Button>
-                </footer>
-            </Container>
-        </div>
+        //     <Container className='modal'>
+        //         <header className='header'>
+        //             <h2>{props.title}</h2>
+        //         </header>
+        //         <div>
+        //             <p className='content'>{props.content}</p>
+        //         </div>
+        //         <footer className="actions">
+        //             <Button onClick={props.onClick}>okay</Button>
+        //         </footer>
+        //     </Container>
+        // </div>
+        <>
+            {ReactDOM.createPortal(
+                <Backdrop />, document.getElementById('backdrop-El')
+            )}
+            {
+                ReactDOM.createPortal(
+                    <Overlay title={props.title} content={props.content} onClick={props.onClick} />, document.getElementById('overlay-El')
+                )
+            }
+        </>
     )
 }
 
